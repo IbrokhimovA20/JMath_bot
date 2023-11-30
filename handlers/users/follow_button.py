5f7555153358dc8208959cbd93479fa729b5a58e
Akhrorbek Ibrokhimov <ibrokhimovakhrorbek20@gmail.com>
2023-11-30 15:05:15 +0500

    removed platinum keyboard

---
+++ handlers/users/follow_button.py
@@ -0,0 +1,12 @@
+from audioop import add
+from cgitb import text
+from gc import callbacks
+from unicodedata import category
+from unittest.mock import call
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from keyboards.inline.callback_data import follow_callback
+
+follow_inline_button = InlineKeyboardMarkup(row_width=1)
+follow_inline_button.insert(InlineKeyboardButton(text='Подписаться↗️', url='https://t.me/J_M_ath'))
+# follow_inline_button.insert(InlineKeyboardButton(text='Подписаться↗️', url='https://t.me/Университеты🎓_school_tashkent'))
+follow_inline_button.insert(InlineKeyboardButton(text='Подписался/ась🟢', callback_data=follow_callback.new(item_name='followed')))
